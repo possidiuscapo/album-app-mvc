7d5996812a477ad0b6c62aa918520fed50c0c866
Possidius Capo <possidiusc@gmail.com>
2023-09-04 10:32:02 +0100

    Code d'ajout d'albums

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 define('APP_ROOT', dirname(__DIR__));
 require "../config/config.php";
 // require "../app/models/AlbumModel.php";
@@ -7,17 +9,24 @@ require "../app/controllers/AlbumController.php";
 $controller = new AlbumController();
 
 // echo APP_ROOT;
-$request = $_SERVER['REQUEST_URI'];
-
-$queryString = strpos($request, '?');
+/**$queryString = strpos($request, '?');
 if($queryString){
     echo (strstr($request, 'action='));
-}
+}*/ 
+
+$request = $_SERVER['REQUEST_URI'];
 
 // $controller->store($_POST['title'],$_POST['artiste']);
 
 if($request === '/'){
     $controller->home();
+}elseif ($request === '/edit') {
+}elseif ($request === '/update') {
+}elseif ($request === '/delete') {
+}elseif ($request === '/add') {
+    print_r($_POST);
+    $controller->store($_POST['title'],$_POST['artiste']);
+    echo "envoie en cours";
 }else{
     echo ' ';
     require(APP_ROOT . '/app/views/pageNotFound.phtml');
